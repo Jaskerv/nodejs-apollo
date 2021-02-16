0d449ddb671c4606f4702eef9d746f32710465f6
Jono Yan <jono2496@gmail.com>
2021-02-16 23:50:49 +1300

    updated backend dependencies

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from 'express';
+import { Session, SessionData } from 'express-session';
 import { Redis } from 'ioredis';
 
 export type Context = {
   // eslint-disable-next-line no-undef
-  req : Request & {session: Express.Session}
+  req : Request & {session: Session & Partial<SessionData> & {userId: number}}
   res : Response
   redis: Redis
 }
